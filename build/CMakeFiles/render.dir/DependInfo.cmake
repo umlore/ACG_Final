2a7be07f5b8ccb2814ef452921ae09a80e7e205b
Eugene Umlor <umlore@rpi.edu>
2017-04-08 14:23:01 -0400

    cmake working on linux

---
+++ build/CMakeFiles/render.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/umlore/Documents/ACG/final/src/boundingbox.cpp" "/home/umlore/Documents/ACG/final/build/CMakeFiles/render.dir/boundingbox.cpp.o"
+  "/home/umlore/Documents/ACG/final/src/camera.cpp" "/home/umlore/Documents/ACG/final/build/CMakeFiles/render.dir/camera.cpp.o"
+  "/home/umlore/Documents/ACG/final/src/edge.cpp" "/home/umlore/Documents/ACG/final/build/CMakeFiles/render.dir/edge.cpp.o"
+  "/home/umlore/Documents/ACG/final/src/glCanvas.cpp" "/home/umlore/Documents/ACG/final/build/CMakeFiles/render.dir/glCanvas.cpp.o"
+  "/home/umlore/Documents/ACG/final/src/main.cpp" "/home/umlore/Documents/ACG/final/build/CMakeFiles/render.dir/main.cpp.o"
+  "/home/umlore/Documents/ACG/final/src/mesh.cpp" "/home/umlore/Documents/ACG/final/build/CMakeFiles/render.dir/mesh.cpp.o"
+  "/home/umlore/Documents/ACG/final/src/render.cpp" "/home/umlore/Documents/ACG/final/build/CMakeFiles/render.dir/render.cpp.o"
+  "/home/umlore/Documents/ACG/final/src/utils.cpp" "/home/umlore/Documents/ACG/final/build/CMakeFiles/render.dir/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLM_FORCE_RADIANS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

d9ccb46cef576ec71d90916dcc61b30a58c1d4ee
Andrew Dunetz <duneta@rpi.edu>
2017-04-22 20:18:14 -0400

    Enable depth buffering.

--- src/render.cpp
+++ src/render.cpp
@@ -259,6 +259,7 @@ void Geometry::drawVBOs(const glm::mat4 &ProjectionMatrix,const glm::mat4 &ViewM
 
       
 
+			glEnable(GL_DEPTH_TEST);
       std::cout << "DRAWGEOMETRY START\n";
       for (int i = 0; i < 1; i++) {
         glm::mat4 MVP = ProjectionMatrix * ViewMatrix;
@@ -289,6 +290,7 @@ void Geometry::drawVBOs(const glm::mat4 &ProjectionMatrix,const glm::mat4 &ViewM
       DrawFloor();
 
       glUniform1i(GLCanvas::whichshaderID, 0);
+			glDisable(GL_DEPTH_TEST);
 
 			GLCanvas::drawPost();
     }
